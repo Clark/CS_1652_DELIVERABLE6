b98085432087ebb4f8152426427dba67d8920209
Clark <clarkonicolas@gmail.com>
2015-12-08 13:29:47 -0500

    comment

--- DifficultyTests.java
+++ DifficultyTests.java
@@ -9,6 +9,7 @@ import java.io.File;
 public class DifficultyTests {
 
 	@Test
+	// Ensures the player is able to play on the easy difficulty (easy maze)
 	public void testDifficultyEasy() {
 		File methodOutput = CopsAndRobbers.chooseDifficulty("easy");
 		File expected = new File("easy.txt");
@@ -16,6 +17,7 @@ public class DifficultyTests {
 	}
 
 	@Test
+	// Ensures the player is able to play on the hard difficulty (hard maze)
 	public void testDifficultyHard() {
 		File methodOutput = CopsAndRobbers.chooseDifficulty("hard");
 		File expected = new File("hard.txt");
@@ -23,6 +25,7 @@ public class DifficultyTests {
 	}
 
 	@Test
+	// Ensures the difficulty is set to easy when player fails to enter corrent input (easy maze)
 	public void testDifficultyIncorrect() {
 		File methodOutput = CopsAndRobbers.chooseDifficulty("blargh");
 		File expected = new File("easy.txt");
